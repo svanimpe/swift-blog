eff8a8edc45df3c841f941b667bbbdfd8b268f5c
Steven Van Impe <steven.vanimpe@icloud.com>
2018-04-25 08:57:52 +0200

    Update Swift to 4.1 and update dependency versions.

--- Package.swift
+++ Package.swift
@@ -4,10 +4,10 @@ import PackageDescription
 let package = Package(
     name: "swift-blog",
     dependencies: [
-        .package(url: "https://github.com/IBM-Swift/HeliumLogger.git", from: "1.0.0"),
-        .package(url: "https://github.com/IBM-Swift/Kitura.git", from: "2.0.0"),
-        .package(url: "https://github.com/IBM-Swift/Kitura-StencilTemplateEngine.git", from: "1.0.0"),
-        .package(url: "https://github.com/IBM-Swift/Swift-cfenv.git", from: "4.0.0")
+        .package(url: "https://github.com/IBM-Swift/HeliumLogger.git", .upToNextMinor(from: "1.7.0")),
+        .package(url: "https://github.com/IBM-Swift/Kitura.git", .upToNextMinor(from: "2.3.0")),
+        .package(url: "https://github.com/IBM-Swift/Kitura-StencilTemplateEngine.git", .upToNextMinor(from: "1.9.0")),
+        .package(url: "https://github.com/IBM-Swift/Swift-cfenv.git", .upToNextMinor(from: "6.0.0"))
     ],
     targets: [
         .target(name: "swift-blog", dependencies: [
