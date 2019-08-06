b28324a9d613792505ca752c7ebb3a9f68b3dab2
Steven Van Impe <steven.vanimpe@icloud.com>
2019-08-06 12:25:41 +0200

    Update versions to Swift 5.

--- Package.swift
+++ Package.swift
@@ -1,13 +1,13 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.0
 import PackageDescription
 
 let package = Package(
     name: "swift-blog",
     dependencies: [
-        .package(url: "https://github.com/IBM-Swift/HeliumLogger.git", .upToNextMinor(from: "1.7.0")),
-        .package(url: "https://github.com/IBM-Swift/Kitura.git", .upToNextMinor(from: "2.5.0")),
-        .package(url: "https://github.com/IBM-Swift/Kitura-StencilTemplateEngine.git", .upToNextMinor(from: "1.10.0")),
-        .package(url: "https://github.com/IBM-Swift/Swift-cfenv.git", .upToNextMinor(from: "6.0.0"))
+        .package(url: "https://github.com/IBM-Swift/HeliumLogger.git", from: "1.9.0"),
+        .package(url: "https://github.com/IBM-Swift/Kitura.git", from: "2.7.0"),
+        .package(url: "https://github.com/IBM-Swift/Kitura-StencilTemplateEngine.git", from: "1.11.0"),
+        .package(url: "https://github.com/IBM-Swift/Swift-cfenv.git", from: "6.0.0")
     ],
     targets: [
         .target(name: "swift-blog", dependencies: [
